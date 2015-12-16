11322ad931551223bde74ebaf982ddbd80fff3d1
hooriza <hooriza.com@linecorp.com>
2015-12-16 10:49:06 +0900

    add files

---
+++ src/zepto-fastclick.js
@@ -0,0 +1,118 @@
+// (jQuery||Zepto).fastClick
+(function($) {
+
+	var config = {
+		tapHoldDuration : 1000,
+		consecutiveInterval : 500,
+		moveThreshold : 10,
+		activableSelector : 'button, a',
+		activedClassName : 'actived'
+	};
+
+	var info = null;
+	var prevents = [];
+
+	var consecutiveTarget, consecutiveTime = -Infinity, consecutiveCounts;
+
+	var $document = $(document);
+
+	var findPrevent = function(target) {
+		for (var i = 0, prevent; !!(prevent = prevents[i]); i++) {
+			if (prevent.target === target) {
+				prevents.splice(i, 1);
+				return prevent;
+			}
+		}
+		return null;
+	};
+
+	var clearInfo = function() {
+		if (!info) { return; }
+		
+		if (info.timer) { clearTimeout(info.timer); }
+		info.$button.removeClass(config.activedClassName);
+		info = null;
+	};
+
+	$document.on('touchstart', function(evt) {
+
+		var touch = (evt.originalEvent || evt).touches[0];
+
+		var $target = $(touch.target);
+		var $button = $target.is(config.activableSelector) ? $target : $target.closest(config.activableSelector);
+
+		info = {
+			pos : [ touch.pageX, touch.pageY ],
+			$target : $target,
+			$button : $button
+		};
+
+		info.timer = setTimeout(function() {
+			info.$target.trigger('taphold');
+			clearInfo();
+		}, config.tapHoldDuration);
+
+		$button.addClass(config.activedClassName);
+
+	}).on('touchmove', function(evt) {
+
+		if (!info) { return; }
+
+		var touch = (evt.originalEvent || evt).touches[0];
+		var distance = Math.pow(info.pos[0] - touch.pageX, 2) + Math.pow(info.pos[1] - touch.pageY, 2);
+
+		if (distance >= 25) {
+			clearInfo();
+		}
+
+	}).on('touchend', function(evt) {
+
+		if (!info) { return; }
+
+		var target = info.$target[0];
+		var customEvent = $.Event('fastclick');
+
+		if (Date.now() - consecutiveTime > config.consecutiveInterval || target !== consecutiveTarget) { consecutiveCounts = 0; }
+
+		customEvent.detail = ++consecutiveCounts;
+		info.$target.trigger(customEvent);
+
+		findPrevent(target);
+
+		consecutiveTime = Date.now();
+		consecutiveTarget = target;
+
+		var prevented = customEvent.isDefaultPrevented();
+
+		prevents.push({
+			target : target,
+			prevented : prevented
+		});
+
+		clearInfo();
+
+		if (prevented) {
+			evt.preventDefault();
+		}
+
+	}).on('touchcancel', function(evt) {
+
+		if (!info) { return; }
+		clearInfo();
+
+	}).on('click', function(evt) {
+
+		var prevent = findPrevent(evt.target);
+		if (prevent && prevent.prevented) {
+			evt.preventDefault();
+		}
+
+	});
+
+	// 스크롤을 멈추려고 눌렀을때 클릭되는 문제
+	document.addEventListener('scroll', clearInfo, true);
+
+	$.fastClick = config;
+	return $.fastClick;
+
+})(window.jQuery||window.Zepto);
